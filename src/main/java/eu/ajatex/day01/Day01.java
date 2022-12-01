d87d82cb457c7bca0a1fc8bb2103a1cf9ff0fce9
alexzh16 <alexzh@seznam.cz>
2022-12-01 17:37:56 +0100

    n

--- src/main/java/eu/ajatex/day01/Day01.java
+++ src/main/java/eu/ajatex/day01/Day01.java
@@ -1,7 +1,12 @@
 package eu.ajatex.day01;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Day01 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Files.readAllLines(Path.of("./src/main/java/eu/ajatex/day01/input.txt"));
         System.out.println("Hello world!");
     }
 }
